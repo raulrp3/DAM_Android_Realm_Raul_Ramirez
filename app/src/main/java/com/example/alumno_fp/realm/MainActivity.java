763eff33acd0ca8f469562db0a277e67f1fdb8b6
raulrp3 <raul.ramirez@escuelaestech.es>
2019-04-23 10:24:24 +0200

    update

--- app/src/main/java/com/example/alumno_fp/realm/MainActivity.java
+++ app/src/main/java/com/example/alumno_fp/realm/MainActivity.java
@@ -56,6 +56,14 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
                 insertPlace();
             }
         });
+
+        placeAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Place place = places.get(rvPlaces.getChildAdapterPosition(v));
+                updatePlace(place);
+            }
+        });
     }
 
     private void insertPlace(){
@@ -74,6 +82,45 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
                 String country = etCountry.getText().toString().trim();
                 if (validate(name) && validate(country)){
                     insert(name,country);
+                }else{
+                    Toast.makeText(getApplicationContext(),"¡Campos obligatorios!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void updatePlace(final Place place){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        final View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.place_dialog, null);
+        builder.setView(view);
+
+        final EditText etName = view.findViewById(R.id.etName);
+        final EditText etCountry = view.findViewById(R.id.etCountry);
+
+        builder.setMessage("Modificar el lugar");
+        etName.setText(place.getName());
+        etCountry.setText(place.getCountry());
+
+        etName.setSelection(etName.getText().length());
+        etCountry.setSelection(etCountry.getText().length());
+
+        builder.setPositiveButton("Modificar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String name = etName.getText().toString().trim();
+                String country = etCountry.getText().toString().trim();
+                if (validate(name) && validate(country)){
+                    update(name,country,place);
                 }else{
                     Toast.makeText(getApplicationContext(),"¡Campos obligatorios!",Toast.LENGTH_SHORT).show();
                 }
@@ -114,5 +161,12 @@ public class MainActivity extends AppCompatActivity implements RealmChangeListen
 
     public void select(){
         places = realm.where(Place.class).findAll();
+    }
+
+    public void update(String name,String country,Place place){
+        realm.beginTransaction();
+        place.setName(name);
+        place.setCountry(country);
+        realm.commitTransaction();
     }
 }
